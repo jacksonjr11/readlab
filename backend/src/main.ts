f0a3aac8ca8e5e2d962ea89ffcce9697c304e2fe
Jackson Augusto <jackson@redeis.app.br>
2026-07-29 22:43:05 -0300

    Added swagger library for api documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('READLAB')
+    .setDescription('The READLAB API documentation')
+    .setVersion('1.0')
+    .addTag('readlab')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
